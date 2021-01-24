9bc7301a01649d31b0a775bd911813a6ddc9079c
João Cabrita <kewne@protonmail.com>
2021-01-24 16:29:35 +0000

    Story: Added exercise list.
    I'm starting off by showing a simple exercise list.
    
    I'm not going to bother creating modules because
    there's so little to show.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-const App = () => <h1>Hello World</h1>
+const ListItem = ({ item }) => <li>{item}</li>
+
+const List = ({ items }) => items.map(item => <ListItem item={item} />)
+
+const exercises = [ "Back Squat", "Run", "Push-up" ]
+
+const App = () => <List items={exercises} />
 
 const element = document.querySelector('#app');
 ReactDOM.render(<App />, element);
